cd57527213fd8e5702943da0f7ceea6f9dc6a974
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-04-27 15:03:18 +0100

    Add files via upload

--- polybar/scripts/nvidia.sh
+++ polybar/scripts/nvidia.sh
@@ -11,7 +11,7 @@ get_gpu
 if (("$TEMP" > 75)); then
    echo "%{F#00ff6a}GPU%{F#f2f2f2} $GPU% %{F#FF8B42}$TEMP°C "
    elif (("$TEMP" > 65)); then
-   echo "%{F#00ff6a}GPU%{F#f2f2f2} $GPU% %{F#ffae00}$TEMP°C "
+   echo "%{F#00ff6a}GPU%{F#f2f2f2} $GPU% %{F#77f2f2f2}$TEMP°C %{F#ffae00}"
 else  
    echo "%{F#00ff6a}GPU%{F#f2f2f2} $GPU% %{F#77f2f2f2}$TEMP°C"       
 fi 
