97394b0e35cc8e0462ab41be519aa4fb0d0480f2
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-04-27 17:10:16 +0100

    Add files via upload

--- polybar/scripts/info-trash.sh
+++ polybar/scripts/info-trash.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 get_trash() { 
-TRASH=$(du -a $HOME/.local/share/Trash/files/* | wc -l ); }
+CHECK=$(du -a $HOME/.local/share/Trash/files/ | wc -l );
+TRASH=$(expr "$CHECK" - 1);  }
 
 get_trash
 
@@ -11,7 +12,7 @@ if (("$TRASH" > 200000 )); then
    elif (("$TRASH" > 0)); then
    echo "%{F#FF8B42} %{F#f2f2f2}Full %{F#77f2f2f2}$TRASH"
 else  
-   echo "%{F#5DC0FE}%{F#f2f2f2} %{F#77f2f2f2}Empty"      
+   echo "%{F#5DC0FE}%{F#f2f2f2}Empty %{F#77f2f2f2} $X"      
 fi 
 
 
