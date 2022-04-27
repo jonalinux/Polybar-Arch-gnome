a16f7785e751a8913d5ae6495a2ef81e46cd3642
Jonathan Sanfilippo <103053714+JonathanSanfilippo@users.noreply.github.com>
2022-04-27 16:01:10 +0100

    Add files via upload

--- polybar/scripts/info-trash.sh
+++ polybar/scripts/info-trash.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
 get_trash() { 
-TRASH=$(du -a ~/.local/share/Trash/files/* | wc -l ); 
-
-}
+TRASH=$(du -a $HOME/.local/share/Trash/files/* | wc -l ); }
 
 get_trash
 
 
-while true; do
+if (("$TRASH" > 200000 )); then
+    echo "%{F#fc1732}%{F#f2f2f2}Very Full %{F#77f2f2f2}$TRASH"
+   elif (("$TRASH" > 0)); then
+   echo "%{F#FF8B42} %{F#f2f2f2}Full %{F#77f2f2f2}$TRASH"
+else  
+   echo "%{F#5DC0FE}%{F#f2f2f2} %{F#77f2f2f2}Empty"      
+fi 
 
 
-while (( TRASH > 0 )); do
-        if (( TRASH  == 1 )); then
-            echo "%{F#fc1732}%{F#f2f2f2}$TRASH Files"
-       elif (( TRASH  > 1 )); then
-            echo "%{F#fc1732}%{F#f2f2f2}$TRASH Files"
-        else
-          echo "%{F#5DC0FE}%{F#f2f2f2}$TRASH Files"
-      fi
-      sleep 10
-       get_trash
-   done
-    
-    while (( TRASH == 0 )); do
-       echo "%{F#5DC0FE}%{F#f2f2f2}$TRASH Files"
-       sleep 10
-       get_trash
-    done
-done
